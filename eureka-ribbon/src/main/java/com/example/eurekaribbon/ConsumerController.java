9aee98926b6ba13197bb1c51fc253d3c55d47e6b
wangyanrun <wangyanrun@weyao.com>
2018-05-08 19:13:44 +0800

    first commit

---
+++ eureka-ribbon/src/main/java/com/example/eurekaribbon/ConsumerController.java
@@ -0,0 +1,23 @@
+package com.example.eurekaribbon;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @classname: ConsumerController
+ * @description:
+ * @author: Ryan
+ * @createdate: 2018-05-8 19: 02
+ **/
+@RestController
+public class ConsumerController {
+    @Autowired
+    RestTemplate restTemplate;
+    @RequestMapping(value = "/ribbon-consumer",method = RequestMethod.GET)
+    public String helloController() {
+        return restTemplate.getForEntity("http://HELLO-SERVICE/hello", String.class).getBody();
+    }
+}
